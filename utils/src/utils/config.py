940349df971899ec4ddeefb0f1c5e9e4f3c887d5
MoMakkawi <MoMakkawi@hotmail.com>
2025-10-14 00:21:57 +0200

    Update the Configs

--- utils/src/utils/config.py
+++ utils/src/utils/config.py
@@ -11,4 +11,5 @@ load_dotenv(dotenv_path=env_path)
 
 class Config:
     DATA_URL = os.getenv("DATA_URL")
-    SAVE_PATH = os.getenv("SAVE_PATH")
+    ORGINAL_DATA_PATH = os.getenv("ORGINAL_DATA_PATH")
+    CLEAN_DATA_PATH = os.getenv("CLEAN_DATA_PATH")
