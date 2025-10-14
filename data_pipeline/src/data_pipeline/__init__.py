311441372dfe5f19369df991298896801d7af68f
MoMakkawi <MoMakkawi@hotmail.com>
2025-10-14 13:18:07 +0200

    Add "Preprocessor" to reach from another package.

--- data_pipeline/src/data_pipeline/__init__.py
+++ data_pipeline/src/data_pipeline/__init__.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: 2025-present MoMakkawi <MoMakkawi@hotmail.com>
 #
 # SPDX-License-Identifier: MIT
-from .fetch_data import fetch_data
 
-__all__ = ["fetch_data"]
+from .fetch_data import fetch_data
+from .preprocess import Preprocessor
+
+__all__ = ["fetch_data", "Preprocessor"]
