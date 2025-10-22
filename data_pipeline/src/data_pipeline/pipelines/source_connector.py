56d870e66d2a74a39b1534b312c05f52367d099c
MoMakkawi <MoMakkawi@hotmail.com>
2025-10-22 21:17:32 +0200

    Rename & Refactor data fetch -> source connector

--- data_pipeline/src/data_pipeline/pipelines/source_connector.py
+++ data_pipeline/src/data_pipeline/pipelines/source_connector.py
@@ -1,20 +1,22 @@
 import requests
 import os
-from utils import Config, logger
+from utils import logger, get_config, load_config
+
+load_config()
 
 def fetch_data(data_url=None, data_path=None):
-    data_url = data_url or Config.DATA_URL
-    data_path = data_path or Config.ORGINAL_DATA_PATH
+    data_url = data_url or get_config().SOURCE_DATA_URL
+    data_path = data_path or get_config().FETCHED_DATA_PATH
     
     # Validate required configuration
     if not data_url:
-        raise ValueError("data_url must be provided either as argument or in Config.DATA_URL")
+        raise ValueError("data_url must be provided either as argument or in get_config().SOURCE_DATA_URL")
     if not data_path:
-        raise ValueError("data_path must be provided either as argument or in Config.data_path")
+        raise ValueError("data_path must be provided either as argument or in get_config().FETCHED_DATA_PATH")
 
     try:
         dir_path = os.path.dirname(data_path)
-        if dir_path:  # Only create directory if path includes one
+        if dir_path:
             os.makedirs(dir_path, exist_ok=True)
         
         response = requests.get(data_url, timeout=60)
@@ -22,6 +24,8 @@ def fetch_data(data_url=None, data_path=None):
         
         with open(data_path, "wb") as f:
             f.write(response.content)
+
+        return data_path
         
         logger.info(f"Data fetched successfully and saved to {data_path}")
     except requests.RequestException as e:
