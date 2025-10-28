583aa08dd39557d2ee944fd07ac1e99db9409d8f
MoMakkawi <MoMakkawi@hotmail.com>
2025-10-28 23:22:03 +0100

    add about and init files

---
+++ data_validation/src/agent/__init__.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2025-present MoMakkawi <MoMakkawi@hotmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+"""Agent module for data validation."""
+
+from .agent import *
+from .tools import *
+
+__all__ = [
+    'device_validation_tool',
+    'timestamp_validation_tool', 
+    'identity_validation_tool',
+]
+
+__all__ = []
