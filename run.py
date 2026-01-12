f9abbb654280edcb57248e4412cff5dfcca8ef4d
MoMakkawi <MoMakkawi@hotmail.com>
2026-01-12 10:18:10 +0100

    Remove print Available models from ragarenn instarctions.

--- run.py
+++ run.py
@@ -53,22 +53,4 @@ def run_parallel():
         logger.info("Cleanup complete.")
 
 if __name__ == "__main__":
-    
-    from openai import OpenAI
-    from utils import Secrets
-
-    client = OpenAI(
-        base_url="https://ragarenn.eskemm-numerique.fr/sso/ch@t/api",
-        api_key= Secrets.RENNES_API_KEY
-    )
-
-    # List available models from ragarenn
-    try:
-        models = client.models.list()
-        print("Available models from ragarenn:")
-        for model in models.data:
-            print(model.id)
-    except Exception as e:
-        print(f"Error fetching models: {str(e)}")
-        
     run_parallel()
