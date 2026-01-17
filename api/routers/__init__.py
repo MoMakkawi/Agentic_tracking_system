801d2161018cb1716279f1743024cd87f810b8ca
MoMakkawi <MoMakkawi@hotmail.com>
2026-01-17 13:48:28 +0100

    Refactoring.

--- api/routers/__init__.py
+++ api/routers/__init__.py
@@ -5,4 +5,12 @@ from .agent import router as agent_router
 from .chat import router as chat_router
 from .analytics import router as analytics_router
 
-__all__ = ["attendance_router", "alerts_router", "groups_router", "agent_router", "chat_router", "analytics_router"]
+__all__ = [
+    "attendance_router",
+    "alerts_router",
+    "groups_router",
+    "agent_router",
+    "chat_router",
+    "analytics_router",
+]
+
